0660b128b8f8623fdc954ff31ed1ea13e1d94314
Misa <dieuhuongngo@gmail.com>
2020-12-01 21:51:26 +0200

    show what's in state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { ShoppingItem } from './store/models/shopping-item.models';
+import { AppState } from './store/models/app-state.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'shopping-list-ngrx-store';
+export class AppComponent implements OnInit {
+
+  shoppingItems$: Observable<Array<ShoppingItem>>;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit(): void {
+    this.shoppingItems$ = this.store.select(store => store.shopping);
+  }
 }
--- src/app/store/reducers/shopping.reducer.ts
+++ src/app/store/reducers/shopping.reducer.ts
@@ -5,6 +5,10 @@ const initialState: Array<ShoppingItem> = [
   {
     id: '1775935f-36fd-467e-a667-09f95b917f6d',
     name: 'Diet Coke',
+  },
+  {
+    id: '1775935f-36fd-467e-a667-0i83275fjhk2',
+    name: 'Berry Pepsi',
   }
 ];
 
