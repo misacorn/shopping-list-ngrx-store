218d2e91c55a15d99f1439ded5a1e1681fbbbeed
Misa <dieuhuongngo@gmail.com>
2020-12-01 22:15:08 +0200

    add new item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { v4 as uuid } from 'uuid';
 
 import { ShoppingItem } from './store/models/shopping-item.models';
 import { AppState } from './store/models/app-state.model';
+import { AddItemAction } from './store/actions/shopping.actions';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +13,18 @@ import { AppState } from './store/models/app-state.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
   shoppingItems$: Observable<Array<ShoppingItem>>;
+  newShoppingItem: ShoppingItem = { id: '', name: '' }
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
     this.shoppingItems$ = this.store.select(store => store.shopping);
+  }
+
+  addItem() {
+    this.newShoppingItem.id = uuid();
+    this.store.dispatch(new AddItemAction(this.newShoppingItem));
+    this.newShoppingItem = { id: '', name: '' };
   }
 }
